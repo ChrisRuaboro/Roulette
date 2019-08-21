126617a726fd5f92d712dba1554eaf7c285ab25a
wwstudent <wwstudent@MS-9D12CH2>
2019-08-21 15:22:20 -0700

    Add project files.

---
+++ RouletteWheel/Board.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RouletteWheel
+{
+    public class Board : Bets , Iattributes
+    {
+        static int number = 0;
+        public static int SpinWheel()
+        {
+            Random r = new Random();
+            number = numbers[r.Next(0, numbers.Length)];
+            return number;
+        }
+        public Color color {get => getColor(number);set => color = value;}
+        public bool? even { get=>isEven(number); set=>even=value; }
+        public bool? low { get => isLows(number); set => low = value; }
+
+        static Color getColor(int number)
+        {
+            Color color = new Color();
+            if (number == 0 || number == 37)
+            {
+                color = Color.green;
+                return color;
+            }
+            foreach (var element in reds)
+            {
+                if (element == number)
+                {
+                    color = Color.red;
+                }
+                else
+                {
+                    color = Color.black;
+                }
+            }
+            
+            return color;
+        }
+        static bool? isEven(int number)
+        {
+            bool? even;
+            if (number == 0 || number == 37)
+            {
+                even = null;
+                return even;
+            }
+            foreach (var element in evens)
+            {
+                if (element == number)
+                {
+                    even = true;
+                }
+                else
+                {
+                    even = false;
+                }
+            }
+            return even;
+
+        }
+        static bool isLows(int number)
+        {
+
+        }
+        static bool isStreets(int number)
+        {
+
+        }
+        static bool isDozens(int number)
+        {
+
+        }
+        static bool isColumns(int number)
+        {
+
+        }
+        static bool isSixNums(int number)
+        {
+
+        }
+        static bool isSplits(int number)
+        {
+
+        }
+        static bool isCorners(int number)
+        {
+
+        }
+
+
+
+
+    }
+
+}
---
+++ RouletteWheel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using RouletteWheel;
+
+namespace RouletteWheel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title();
+            Console.WriteLine(Bets.zeros[1]);
+        }
+        static void Title()
+        {
+            string title = 
+            
+                @" ________  ________  ___  ___  ___       _______  _________  _________  _______      "+"\n"+
+                @"|\   __  \|\   __  \|\  \|\  \|\  \     |\  ___ \|\___   ___\\___   ___\\  ___ \     "+"\n"+
+                @"\ \  \|\  \ \  \|\  \ \  \\\  \ \  \    \ \   __/\|___ \  \_\|___ \  \_\ \   __/|    "+"\n"+
+                @" \ \   _  _\ \  \\\  \ \  \\\  \ \  \    \ \  \_|/__  \ \  \     \ \  \ \ \  \_|/__  "+"\n"+
+                @"  \ \  \\  \\ \  \\\  \ \  \\\  \ \  \____\ \  \_|\ \  \ \  \     \ \  \ \ \  \_|\ \ "+"\n"+
+                @"   \ \__\\ _\\ \_______\ \_______\ \_______\ \_______\  \ \__\     \ \__\ \ \_______\"+"\n"+
+                @"    \|__|\|__|\|_______|\|_______|\|_______|\|_______|   \|__|      \|__|  \|_______|"
+            ;
+            Console.WriteLine(title);
+        }
+        static void askToSpinWheel()
+        {
+            Console.WriteLine("Now its time to Spin! Press enter when ready");
+
+        }
+    }
+}
